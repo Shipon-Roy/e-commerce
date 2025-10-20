d65ad6a615821a7ad7fc5d83ce6aff4f7d8cb5cc
Shipon-Roy <shiponroy509@gmail.com>
2025-10-20 13:31:53 +0600

    footer update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,13 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="bg-gray-800 text-gray-400 text-center p-5">
-      my first change
+      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2025{" "}
+        <a href="#" class="hover:underline">
+          E-commerce™
+        </a>
+        . All Rights Reserved. create by shipon
+      </span>
     </div>
   );
 }
