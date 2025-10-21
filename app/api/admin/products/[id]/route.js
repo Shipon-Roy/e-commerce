369654c6933a38ce8d5a654a5d51edb6c9dbb75e
Shipon-Roy <shiponroy509@gmail.com>
2025-10-21 11:58:12 +0600

    work

--- app/api/admin/products/[id]/route.js
+++ app/api/admin/products/[id]/route.js
@@ -2,19 +2,21 @@ import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import Product from "@/lib/models/Product";
 
-export async function PUT(req, { params }) {
+export async function PUT(req, context) {
   await dbConnect();
-  const { id } = params;
+
+  const { id } = await context.params; // ✅ await params
   const data = await req.json();
 
   const updated = await Product.findByIdAndUpdate(id, data, { new: true });
   return NextResponse.json(updated);
 }
 
-export async function DELETE(req, { params }) {
+export async function DELETE(req, context) {
   await dbConnect();
-  const { id } = params;
 
+  const { id } = await context.params; // ✅ await params
   await Product.findByIdAndDelete(id);
+
   return NextResponse.json({ success: true });
 }
